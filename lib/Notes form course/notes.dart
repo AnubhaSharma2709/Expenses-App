1286de03e5ebae2f75a27c348f979c3d37e53b8b
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-02-11 22:52:24 +0530

    commit

---
+++ lib/Notes form course/notes.dart
@@ -0,0 +1,30 @@
+// Important widgets:
+//1.Material App
+//2.Scaffold/CupertinoPageScaffold
+
+//Important Layout
+//Rows
+//Container
+//Columns
+
+//Row/Column Children
+//1.Flexible
+//2.Expanded
+
+//stack Widget
+//card widget-drop shadow
+
+//repeated element
+//1.list view
+//2.grid view
+//3.list tile
+
+//content types
+//1.Text
+//2.Image
+//3.icon
+
+//user input widget
+//1.Text Field
+//2.raised Button/Flat Button
+//3.Gesture Detector/ink well
